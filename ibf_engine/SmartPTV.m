d62209e3f8e2cad072884f7ab9585b4e3489d881
Tianyi <tianyiwang666@gmail.com>
2020-02-21 11:37:08 -0500

    Update SmartPTV.m

--- ibf_engine/SmartPTV.m
+++ ibf_engine/SmartPTV.m
@@ -1,3 +1,18 @@
 function smartPTV = SmartPTV(x)
+% Function:
+%   smartPTV = SmartPTV(x)
+%
+% Input:
+%   x: the array that need to calculate PV
+%
+% Output:
+%   smartPTV: the scalar PV value of x
+%
+% Purpose:
+%   Compute PV value without considering any NaNs
+%
+% Info:
+%   Contact: huanglei0114@gmail.com (Dr. HUANG Lei), tianyiwang666@gmail.com (Dr WANG Tianyi)
+%   Copyright reserved.
 
 smartPTV = range(x(isfinite(x)));
